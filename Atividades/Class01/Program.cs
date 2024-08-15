f04a3bce2bd24e06c7133ac9c45540fb7eee8489
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2024-08-14 22:07:14 -0300

    First Project .net

---
+++ Atividades/Class01/Program.cs
@@ -0,0 +1,15 @@
+﻿//Comentário de uma linha
+/*
+    Comentário de múltiplas linhas
+*/
+
+//Declarando variável
+int number;
+//Atribuindo valor
+number = 10;
+
+//Declarando constante
+const int DAYS_IN_WEEK = 7;
+
+//Imprimindo String Concatenada
+Console.WriteLine( $"A semana tem {DAYS_IN_WEEK} dias");
