5aceb5a99d14440bcfd3aba2f2218a804069282c
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2024-09-04 20:26:36 -0300

    Gracias Thiago

---
+++ Atividades/multidimensional-array/Program.cs
@@ -0,0 +1,29 @@
+﻿// Cruiando um vetor multidimensional com 5 linhas e 2 colunas
+
+int[,] numbers = new int[5,2];          
+
+// Criado um vetor tridimensionnal
+int[,,]triNumbers = new int[5,4,3];
+
+// Podemos incializar os valores do vetor no momento de sua declaração
+int[,] iniNumbers = new int[,]
+{
+    {9,15,-9},
+    {19,25,-90},
+    {29,35,-91},
+    {39,45,-92},
+    {49,55,-93},
+};
+
+// Para acessar os valores contidos neste tipo de variável
+int valor = iniNumbers[0,1]; //Neste caso = 15
+
+Console.WriteLine( $"iniNumbers[0,1] == {valor}" );
+
+//Escreva um código que identifique o maior e o menor número, descrevendo a sua posição dentro do vetor
+int maior = 0, menor, i;
+for (iniNumbers = 0, iniNumbers > 15, iniNumbers++ GetLangh)
+{
+    for (iniNumbers = 0, iniNumbers > 15, iniNumbers++ GetLangh)
+    
+}
---
+++ Tarefas/Vetor Ímpar-Par/Program.cs
@@ -0,0 +1,9 @@
+﻿//Escreva um algoritmo que armazene em dois arrays separados os primeiros 100 números pares e ímpares
+//No final, imprima a sequência armazenada em cada um deles.
+#include <stdio.h>
+int main()
+{
+    int pares[100]
+    int impares[100]
+    int indicearrayp
+}
