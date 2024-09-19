80e42f83bdf111653c6c57778fa21a84f2d2f025
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2024-09-18 22:09:38 -0300

    Att Pilhas Lotérica

--- Tarefas/ListaSenha/Program.cs
+++ Tarefas/ListaSenha/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Crie um programa no qual o mesmo simule uma lotérica, na qual terá uma pilha de senhas, a senha será retirada
+//com a função Peek, na qual terá outra pilha que captará as senhas já usadas e chamará a próxima.
+//Stack = Pilha em Inglês
+
+using ListaSenha;
+
+Stack<int> password = new();
+
+TicketDevelop ticket = new();
+password.Push(27);
+password.Push(28);
+password.Push(29);
+password.Push(30);
