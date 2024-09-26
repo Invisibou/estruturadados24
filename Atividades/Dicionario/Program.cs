826c7cd2255b3cc834aebb43986ca182232bae98
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2024-09-25 22:03:02 -0300

    Hash Map - Dictionary - Incompleto - Email - Japa

---
+++ Atividades/Dicionario/Program.cs
@@ -0,0 +1,73 @@
+﻿// Unlike the HashTable, the Dictionary allows us to specify the data type that will be used in the Key -> Value pairs. This provides us with a strongly typed structure.
+
+using Dicionario;
+
+Dictionary<string, string> dictionary = new Dictionary<string, string>()
+{
+    { "Key 1", "Key 1" },
+    { "Key 2", "Key 2" }
+};
+
+//Get the value of Dictionary
+
+string val = dictionary["Key 1"];
+
+try
+{
+    val = dictionary["Key 3"];
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error trying to get the value by Key 3");
+    System.Console.WriteLine(ex.Message);
+}
+{
+
+}
+// Verify if Key exist before acess to dont came errors
+if ( dictionary.ContainsKey("Key 2"))
+{
+    System.Console.WriteLine($"Key 2 ");
+}
+
+// Another way to get the value and dont get a error
+dictionary.TryGetValue("Key 4", out string? valor);
+if(valor is not null)
+    System.Console.WriteLine($"Key 4: {valor}");
+
+// We can add by the same way with inexistent Key
+dictionary["Key 0"] = "Value 0";
+
+// Run the dictionary
+foreach(KeyValuePair<string, string> kvp in dictionary) //kvp = nome da variável
+{
+    System.Console.WriteLine($"{kvp.Key}:{kvp.Value}");
+}
+
+//Using Abstract Data Types with dicionary
+
+Dictionary<int, Pessoa> dicPessoa = new Dictionary<int, Pessoa>();
+dicPessoa.Add(
+    1,
+    new Pessoa(){
+        Id = 0,
+        BirthDate = new DateTime(1984, 7, 5),
+        Name = "Mauricio Roberto Gonzatto"
+    }
+);
+dicPessoa.Add(
+    2,
+    new Pessoa(){
+        Id = 1,
+        BirthDate = new DateTime(1999, 8, 1),
+        Name = "Maurijua Habilad Antat Uiu"
+    }
+);
+
+foreach ( KeyValuePair<int, Pessoa> kvp in dicPessoa) 
+{
+    Console.WriteLine(kvp.Key);
+    Console.WriteLine(kvp.Value.Id);
+    Console.WriteLine(kvp.Value.Name);
+    Console.WriteLine(kvp.Value.BirthDate);
+}
---
+++ Atividades/HashTable/Program.cs
@@ -0,0 +1,35 @@
+﻿// Exemplo de uma agenda telefônica
+
+using System.Collections;
+
+Hashtable phoneBook = new Hashtable() //Método Construtor, pois tem o mesmo nome da classe, a classe que define o tipo da variável.
+{
+    //{ "Chave", "Valor"},
+    { "Thomazzi", "49-9961-0150" },
+    { "Mauricio Gonzatto", "49-99975-8575" },
+    { "Gabriel Bianchi", "49-99105-8904" } //Como é o último, não precisa de vírgula
+}; // O ";" tem que ir no final, pois é para finalizar sentença, caso fosse nos "()", não leria o resto.
+
+// É possível adicionar elementos de diversas formas pelo próprio índice chave inexistente
+phoneBook["Thiago Padilha"] = "49-99176-8255";
+
+// Ou pelo método Add()
+phoneBook.Add("Marcos Henrique", "49-99202-6169");
+
+//Entretanto, a TabelaHash verifica se há duplicidade e chave e pode lançar uma Exception
+try //Imagina o try-catch como se fosse um "If" "Else", mas tem diferenças.
+{
+    phoneBook.Add("Mauricio Gonzatto", "49-99975-8575");
+}catch(Exception ex)
+{
+    Console.WriteLine($"Erro ao tentar incluir valor repetido.");
+    Console.WriteLine(ex.Message);
+}
+
+//Percorrendo itens da HashTable
+Console.WriteLine("Agenda telefônica:");
+if(phoneBook.Count == 0) //Não precisou de chaves, pois se tiver somente uma função, não precisa, a única função dentro do IF, é o Console.WriteLine.
+    Console.WriteLine("A agenda está vazia.");
+else
+    foreach(DictionaryEntry entry in phoneBook) //A única função dentro do Else, é o foreach.
+            Console.WriteLine($"{entry.Key}:{entry.Value}"); // A única função dentro do foreach, é Console.WriteLine
