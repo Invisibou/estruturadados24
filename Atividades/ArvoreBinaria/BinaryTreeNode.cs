ace3603cc69d72c93c182ae882f974697efeade8
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2024-11-13 21:28:50 -0300

    ArvoresBInariasLoucas

--- Atividades/ArvoreBinaria/BinaryTreeNode.cs
+++ Atividades/ArvoreBinaria/BinaryTreeNode.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ArvoreBinaria
@@ -12,12 +13,13 @@ namespace ArvoreBinaria
             base.Children = 
                 new List<TreeNode<T>>();
         }
+        //Aqui ele atribuiu a árvore binária a possibilidade de ter dois filhos, a partir do left e right, se tivesse só 1 deles, a árvore só poderia ser "manca"
         public BinaryTreeNode<T> Left //Atributo que especifica o nó da árvore da esquerda, o nome é Left.
         {
             get {return (BinaryTreeNode<T>)Children[0]; }
             set {Children[0] = value; }
         }
-                public BinaryTreeNode<T> Right //Atributo que especifica o nó da árvore da esquerda, o nome é Left.
+                public BinaryTreeNode<T> Right //Atributo que especifica o nó da árvore da direita, o nome é right.
         {
             get {return (BinaryTreeNode<T>)Children[1]; }
             set {Children[1] = value; }
--- Atividades/ArvoreBinaria/TreeNode.cs
+++ Atividades/ArvoreBinaria/TreeNode.cs
@@ -5,8 +5,22 @@ using System.Threading.Tasks;
 
 namespace ArvoreBinaria
 {
-    public class TreeNode
+    public class TreeNode<T>
     {
-        
+        public T? Data { get; set; }
+        public TreeNode<T>? Parent { get; set; }  
+        public List<TreeNode<T>>? Children { get; set; }
+
+        public int GetHeight()
+        {
+            int height = 1;
+            TreeNode<T> current = this;
+            while (current.Parent != null)
+            {
+                height++;
+                current = current.Parent;
+            }
+            return height;
+        }
     }
 }
