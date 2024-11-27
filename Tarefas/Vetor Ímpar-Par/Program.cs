20b8a65b824a38df4b697fcf5c86a6124b17221e
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2024-11-27 20:29:34 -0300

    AttVetorÍmparPar

--- Tarefas/Vetor Ímpar-Par/Program.cs
+++ Tarefas/Vetor Ímpar-Par/Program.cs
@@ -1,9 +1,26 @@
 ﻿//Escreva um algoritmo que armazene em dois arrays separados os primeiros 100 números pares e ímpares
 //No final, imprima a sequência armazenada em cada um deles.
-#include <stdio.h>
-int main()
+List<int> numerosPares = [];
+List<int> numerosImpares = [];
+
+for (int i = 0; numerosPares.Count <= 100 && numerosImpares.Count <= 100;  i++)
 {
-    int pares[100]
-    int impares[100]
-    int indicearrayp
+    if (i % 2 == 0)
+    {
+        numerosPares.Add(i);
+    }
+    else
+    {
+        numerosImpares.Add(i);
+    }
 }
+
+Console.Write("Números pares: ");
+foreach (var i in  numerosPares)
+    Console.Write($"{i}, ");
+
+Console.WriteLine();
+Console.WriteLine();
+Console.Write("Números ímpares: ");
+foreach (var i in numerosImpares)
+    Console.Write($"{i}, ");
